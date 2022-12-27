c9599950479931924df85be5922ec0e486c6669e
francesco.pieraccini <francesco.pieraccini@vola.it>
2022-12-27 18:43:56 +0100

    mock

--- src/User.php
+++ src/User.php
@@ -4,9 +4,17 @@ class User
 {
     public $first_name;
     public $surname;
+    public $email;
 
     public function getFullName()
     {
         return trim("{$this->first_name} {$this->surname}");
+    }
+
+    public function notify($message)
+    {
+        $mailer = new Mailer;
+
+        $mailer->sendMessage($this->email, $message);
     }
 }
