5f77423fa18d4451d7c9863378ab994c07c21037
francesco.pieraccini <francesco.pieraccini@vola.it>
2022-12-27 17:42:41 +0100

    fixtures

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -4,41 +4,62 @@ use PHPUnit\Framework\TestCase;
 
 class QueueTest extends TestCase
 {
-    public function testNewQueueIsEmpty()
+    protected $queue;
+
+    protected function setUp(): void
     {
-        $queue = new Queue;
-
-        $this->assertEquals(0, $queue->getCount());
-
-        return $queue;
+        $this->queue =new Queue;
     }
 
-    /**
-     * aggiungo dipendenza per passare la queue già creata
-     * @depends testNewQueueIsEmpty
-     */
-    public function testAnItemIsAddedToTheQueue(Queue $queue)
+    //eseguito alla fine del test
+    protected function tearDown(): void
+    {
+        unset($this->queue);
+    }
+
+    public function testNewQueueIsEmpty()
     {
 //        $queue = new Queue;
 
-        $queue->push('green');
+        $this->assertEquals(0, $this->queue->getCount());
+//        $this->assertEquals(0, $queue->getCount());
 
-        $this->assertEquals(1, $queue->getCount());
-
-        return $queue;
+//        return $queue;
     }
 
     /**
      * aggiungo dipendenza per passare la queue già creata
-     * @depends testAnItemIsAddedToTheQueue
+//     * @depends testNewQueueIsEmpty
      */
-    public function testAnItemIsRemovedFromTheQueue(Queue $queue)
+    public function testAnItemIsAddedToTheQueue()
+//    public function testAnItemIsAddedToTheQueue(Queue $queue)
+    {
+//        $queue = new Queue;
+
+        $this->queue->push('green');
+//        $queue->push('green');
+
+        $this->assertEquals(1, $this->queue->getCount());
+//        $this->assertEquals(1, $queue->getCount());
+
+//        return $queue;
+    }
+
+    /**
+     * aggiungo dipendenza per passare la queue già creata
+//     * @depends testAnItemIsAddedToTheQueue
+     */
+    public function testAnItemIsRemovedFromTheQueue()
+//    public function testAnItemIsRemovedFromTheQueue(Queue $queue)
     {
 //        $queue = new Queue;
 //        $queue->push('green');
-        $item = $queue->pop();
+        $this->queue->push('green');
+        $item = $this->queue->pop();
+//        $item = $queue->pop();
 
-        $this->assertEquals(0, $queue->getCount());
+        $this->assertEquals(0, $this->queue->getCount());
+//        $this->assertEquals(0, $queue->getCount());
 
         $this->assertEquals('green', $item);
     }
