0b3b1598f83ab358f1e5589da4ad40e4ec418521
francesco.pieraccini <francesco.pieraccini@vola.it>
2022-12-30 15:32:40 +0100

    test private mothods

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -25,4 +25,19 @@ class ItemTest extends TestCase
 //
 //        $this->assertIsString($item->getToken());
 //    }
+
+    public function testTokenIsAString()
+    {
+        $item = new Item();
+
+        $reflector = new ReflectionClass(Item::class);
+
+        $method = $reflector->getMethod('getToken');
+
+        $method->setAccessible(true);
+
+        $result = $method->invoke($item);
+
+        $this->assertIsString($result);
+    }
 }
